791f346f69fab20ee4ad536f82722ae888f6c192
lorena112233 <43742389+lorena112233@users.noreply.github.com>
2018-12-07 18:23:28 +0100

    faltaAjusteTablas

--- Look4Events/Look4Events/wwwroot/js/site.js
+++ Look4Events/Look4Events/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// • • • • • • • • • • •RELOJ • • • • • • • • • • •
+function verHora() {
+    let mihora = new Date();
+    let horas = mihora.getHours().toString();
+    let minutos = mihora.getMinutes().toString();
+    if (minutos.length == 1) minutos = "0" + minutos;
+    let segundos = mihora.getSeconds().toString();
+    if (segundos.length == 1) segundos = "0" + segundos;
+    document.forms[0].miReloj.value = horas + " : " + minutos + " : " + segundos;
+}
+// • • • • • • • • • • •
