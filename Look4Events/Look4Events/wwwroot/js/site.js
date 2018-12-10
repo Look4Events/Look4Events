8e750c009d04e3c0f26e79e6e2194e6dc9cba402
lorena112233 <43742389+lorena112233@users.noreply.github.com>
2018-12-10 06:32:58 +0100

    filtroTablaAbout

--- Look4Events/Look4Events/wwwroot/js/site.js
+++ Look4Events/Look4Events/wwwroot/js/site.js
@@ -13,3 +13,16 @@ function verHora() {
     document.forms[0].miReloj.value = horas + " : " + minutos + " : " + segundos;
 }
 // • • • • • • • • • • •
+
+//~~~~~~~~~~~tabla en vista CONTACT:
+$(document).ready(function () {
+    $('#example').DataTable({
+        columnDefs: [
+            {
+                targets: [0, 1, 2],
+                className: 'mdl-data-table__cell--non-numeric'
+            }
+        ]
+
+    });
+});
