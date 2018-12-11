e48df48af8ee162792cd46f6dc678456bc3a52ed
lorena112233 <43742389+lorena112233@users.noreply.github.com>
2018-12-11 17:19:45 +0100

    pte adaptar reposnive a nuevas tablas en "prueba"

--- Look4Events/Look4Events/Controllers/HomeController.cs
+++ Look4Events/Look4Events/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Look4Events.Models;
+using Look4Events.Models.Events;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
 
 namespace Look4Events.Controllers
 {
@@ -13,7 +16,64 @@ namespace Look4Events.Controllers
         //prueba para usar el otro link
         public async Task<IActionResult> Prueba()
         {
-            return View();
+            bool next = true;
+
+            HttpClient client = new HttpClient();
+            int i = 1;
+            List<Event> eventos = new List<Event>();
+            while (next)
+            {
+                HttpResponseMessage response = await client.GetAsync("https://app.ticketmaster.com/discovery/v2/events.json?apikey=h3I9tWkebYWN4j7RUCINFghyZEoQMjMi");
+                if (response.IsSuccessStatusCode)
+                {
+                    string output = await response.Content.ReadAsStringAsync();
+                    JObject sacaJson = JObject.Parse(output);
+
+                    IList<JToken> results = sacaJson["_embedded"]["events"].Children().ToList();
+                    IList<JToken> venues;
+
+                    foreach (JToken result in results)
+                    {
+                        List<Venue> eventVenues = new List<Venue>();
+                        venues = result["_embedded"]["venues"].Children().ToList();
+                        foreach (JToken venue in venues)
+                        {
+                            Venue eventVenue = venue.ToObject<Venue>();
+
+                            JToken locationJson = venue["location"];
+                            Location location = locationJson.ToObject<Location>();
+
+
+                            eventVenues.Add(eventVenue);
+                        }
+
+                        // JToken.ToObject is a helper method that uses JsonSerializer internally
+                        Event searchResult = result.ToObject<Event>();
+                        searchResult.Venues = eventVenues;
+                        eventos.Add(searchResult);
+                    }
+
+                }
+                next = false;
+                //foreach (var item in items.Results)
+                //{
+
+                //    Event p = new Event
+                //    {
+                //        Name = item.Name,
+                //        Type = item.Type,
+                //        Id = item.Id,
+                //        Url = item.Url
+
+                //    };
+
+
+                //    eventos.Add(p);
+                //}
+                //i++;
+            }
+            return View(eventos);
+
         } 
         //#################################################
         public IActionResult Index()
