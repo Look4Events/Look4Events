018e85c7eea913efe811202ba03148a24dbde2df
lorena112233 <43742389+lorena112233@users.noreply.github.com>
2018-12-12 14:13:34 +0100

    falta sacar URL imagen en PRUEBA

--- Look4Events/Look4Events/wwwroot/js/site.js
+++ Look4Events/Look4Events/wwwroot/js/site.js
@@ -3,15 +3,15 @@
 
 // Write your JavaScript code.
 // • • • • • • • • • • •RELOJ • • • • • • • • • • •
-function verHora() {
-    let mihora = new Date();
-    let horas = mihora.getHours().toString();
-    let minutos = mihora.getMinutes().toString();
-    if (minutos.length == 1) minutos = "0" + minutos;
-    let segundos = mihora.getSeconds().toString();
-    if (segundos.length == 1) segundos = "0" + segundos;
-    document.forms[0].miReloj.value = horas + " : " + minutos + " : " + segundos;
-}
+//function verHora() {
+//    let mihora = new Date();
+//    let horas = mihora.getHours().toString();
+//    let minutos = mihora.getMinutes().toString();
+//    if (minutos.length == 1) minutos = "0" + minutos;
+//    let segundos = mihora.getSeconds().toString();
+//    if (segundos.length == 1) segundos = "0" + segundos;
+//    document.forms[0].miReloj.value = horas + " : " + minutos + " : " + segundos;
+//}
 // • • • • • • • • • • •
 
 //~~~~~~~~~~~tabla en vista:
